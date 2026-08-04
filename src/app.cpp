ebcb5b70480144cb34fc905f5afc60aa74bd79b4
uiop <uiop@wasdhjkl.xyz>
2026-08-04 07:52:22 -0400

    app!: gles3
    BREAKING CHANGE: Building without Emscripten not supported rn

--- src/app.cpp
+++ src/app.cpp
@@ -1,8 +1,13 @@
 #include "app.hpp"
 
+#ifndef __EMSCRIPTEN__
+#error "Building with emscripten is required (FIXME)"
+#endif
+
+#include <GLES3/gl3.h>
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_error.h>
-#include <SDL3/SDL_render.h>
+#include <SDL3/SDL_video.h>
 #include <concepts>
 #include <expected>
 #include <format>
@@ -23,7 +28,7 @@ template <auto Fn> struct FnDeleter {
 
 template <typename T, auto Fn> using Handle = std::unique_ptr<T, FnDeleter<Fn>>;
 using WindowPtr = Handle<SDL_Window, SDL_DestroyWindow>;
-using RendererPtr = Handle<SDL_Renderer, SDL_DestroyRenderer>;
+using GLCtxPtr = Handle<SDL_GLContextState, SDL_GL_DestroyContext>;
 
 }; // namespace
 
@@ -34,7 +39,7 @@ struct App::Impl {
     SdlGuard(SdlGuard &&) = delete;
   } sdl_guard;
   WindowPtr window;
-  RendererPtr renderer;
+  GLCtxPtr glctx;
 };
 
 App::App(std::unique_ptr<Impl> p) noexcept : impl{std::move(p)} {}
@@ -44,40 +49,55 @@ App::~App() = default;
 
 std::expected<App, std::string> App::create(const char *title) {
   std::unique_ptr<Impl> impl;
-
   if (!SDL_Init(SDL_INIT_VIDEO)) {
     return std::unexpected{std::format("SDL_Init: {}", SDL_GetError())};
   }
   impl = std::make_unique<Impl>();
 
-  if (!SDL_CreateWindowAndRenderer(title, WIDTH, HEIGHT, SDL_WINDOW_RESIZABLE,
-                                   std::out_ptr(impl->window),
-                                   std::out_ptr(impl->renderer))) {
+  if (!SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK,
+                           SDL_GL_CONTEXT_PROFILE_ES)) {
     return std::unexpected{
-        std::format("SDL_CreateWindowAndRenderer: {}", SDL_GetError())};
-  }
+        std::format("SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK): {}",
+                    SDL_GetError())};
+  };
+  if (!SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3)) {
+    return std::unexpected{
+        std::format("SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION): {}",
+                    SDL_GetError())};
+  };
+  if (!SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 0)) {
+    return std::unexpected{
+        std::format("SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION): {}",
+                    SDL_GetError())};
+  };
 
-  if (!SDL_SetRenderLogicalPresentation(impl->renderer.get(), WIDTH, HEIGHT,
-                                        SDL_LOGICAL_PRESENTATION_LETTERBOX)) {
-    return std::unexpected{
-        std::format("SDL_SetRenderLogicalPresentation: {}", SDL_GetError())};
+  auto window = SDL_CreateWindow(title, WIDTH, HEIGHT, SDL_WINDOW_OPENGL);
+  if (!window) {
+    return std::unexpected{std::format("SDL_CreateWindow: {}", SDL_GetError())};
   }
+  impl->window.reset(window);
+
+  auto glctx = SDL_GL_CreateContext(impl->window.get());
+  if (!glctx) {
+    return std::unexpected{
+        std::format("SDL_GL_CreateContext: {}", SDL_GetError())};
+  }
+  impl->glctx.reset(glctx);
+
+  if (!SDL_GL_SetSwapInterval(1)) {
+    return std::unexpected{
+        std::format("SDL_GL_SetSwapInterval: {}", SDL_GetError())};
+  };
 
   return App{std::move(impl)};
 }
 
 std::expected<void, std::string> App::iterate() {
-  auto rd = impl->renderer.get();
-  if (!SDL_SetRenderDrawColor(rd, 255, 0, 0, SDL_ALPHA_OPAQUE)) {
+  glClearColor(1.0f, 0.0f, 0.0f, 1.0f);
+  glClear(GL_COLOR_BUFFER_BIT);
+  if (!SDL_GL_SwapWindow(impl->window.get())) {
     return std::unexpected{
-        std::format("SDL_SetRenderDrawColor: {}", SDL_GetError())};
-  }
-  if (!SDL_RenderClear(rd)) {
-    return std::unexpected{std::format("SDL_RenderClear: {}", SDL_GetError())};
-  }
-  if (!SDL_RenderPresent(rd)) {
-    return std::unexpected{
-        std::format("SDL_RenderPresent: {}", SDL_GetError())};
+        std::format("SDL_GL_SwapWindow: {}", SDL_GetError())};
   }
   return {};
 }
