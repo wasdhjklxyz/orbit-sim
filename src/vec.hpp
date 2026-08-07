6a77478112477e1a5cf0dca727c257dcfad36952
uiop <uiop@wasdhjkl.xyz>
2026-08-07 01:12:45 -0400

    vec: operators, dot, length^2

--- src/vec.hpp
+++ src/vec.hpp
@@ -13,6 +13,18 @@ template <typename T> struct Vec3 {
     return lhs;
   }
 
+  constexpr Vec3 &operator-=(const Vec3 &rhs) noexcept {
+    x -= rhs.x;
+    y -= rhs.y;
+    z -= rhs.z;
+    return *this;
+  }
+
+  friend constexpr Vec3 operator-(Vec3 lhs, const Vec3 &rhs) noexcept {
+    lhs -= rhs;
+    return lhs;
+  }
+
   constexpr Vec3 &operator*=(T s) noexcept {
     x *= s;
     y *= s;
@@ -30,7 +42,33 @@ template <typename T> struct Vec3 {
     return rhs;
   }
 
+  constexpr Vec3 &operator/=(T s) noexcept {
+    x /= s;
+    y /= s;
+    z /= s;
+    return *this;
+  }
+
+  friend constexpr Vec3 operator/(Vec3 lhs, T s) noexcept {
+    lhs /= s;
+    return lhs;
+  }
+
+  friend constexpr Vec3 operator/(T s, Vec3 rhs) noexcept {
+    rhs /= s;
+    return rhs;
+  }
+
   T x, y, z;
 };
 
 using Vec3d = Vec3<double>;
+
+template <typename T>
+constexpr T dot(const Vec3<T> &a, const Vec3<T> &b) noexcept {
+  return a.x * b.x + a.y * b.y + a.z * b.z;
+}
+
+template <typename T> constexpr T length_squared(const Vec3<T> &v) noexcept {
+  return dot(v, v);
+}
