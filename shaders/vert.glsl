e36e84305148fed68f21dec2a9af82900e7d8415
uiop <uiop@wasdhjkl.xyz>
2026-08-25 01:03:23 -0400

    shaders(vert): remove hue attr

--- shaders/vert.glsl
+++ shaders/vert.glsl
@@ -1,7 +1,6 @@
 #version 300 es
 
 layout (location = 0) in vec3 aPos;
-layout (location = 1) in float aHue;
 
 out vec3 ourColor;
 
@@ -17,6 +16,5 @@ vec3 hue2rgb(float hue) {
 
 void main() {
   gl_Position = vec4(aPos, 1.0);
-  gl_PointSize = 10.0;
-  ourColor = hue2rgb(fract(aHue - uTime / PERIOD));
+  ourColor = hue2rgb(fract(1.0 - uTime / PERIOD));
 }
