5f6db0b32bef81690500f4701f75b68e58878c9d
uiop <uiop@wasdhjkl.xyz>
2026-09-01 17:02:46 -0400

    gfx/geom: circle class

---
+++ src/circle.cpp
@@ -0,0 +1,45 @@
+#include "geom.hpp"
+
+#include <cstddef>
+#include <glm/common.hpp>
+#include <glm/ext/scalar_constants.hpp>
+#include <glm/gtc/constants.hpp>
+#include <glm/trigonometric.hpp>
+#include <glm/vec3.hpp>
+
+namespace gfx::geom {
+
+/* NOTE: If sectors=1 then its just a triangle */
+namespace {
+static constexpr std::size_t MIN_SECTOR_COUNT = 1;
+} // namespace
+
+Circle::Circle(float radius, std::size_t sectors)
+    : radius{glm::max(radius, float(0))},
+      sectors{glm::max(sectors, MIN_SECTOR_COUNT)} {
+  build_vertices();
+  build_indicies();
+}
+
+void Circle::build_vertices() {
+  const float sector_step = glm::two_pi<float>() / sectors;
+  vertices.clear();
+  vertices.push_back({0, 0, 0}); // NOTE: Center
+  for (std::size_t k = 0; k <= sectors; k++) {
+    const float theta = sector_step * k;
+    vertices.push_back(
+        glm::vec3(glm::cos(theta) * radius, glm::sin(theta) * radius, 0));
+  }
+}
+
+void Circle::build_indicies() {
+  indicies.clear();
+  constexpr auto center_idx = 0;
+  for (std::size_t i = 0; i < sectors; i++) {
+    indicies.push_back(i + 1);
+    indicies.push_back((i + 2) % sectors);
+    indicies.push_back(center_idx);
+  }
+}
+
+} // namespace gfx::geom
--- src/geom.hpp
+++ src/geom.hpp
@@ -7,21 +7,38 @@
 
 namespace gfx::geom {
 
-class Sphere {
+class Mesh {
+protected:
+  virtual void build_vertices();
+  virtual void build_indicies();
+  std::vector<glm::vec3> vertices;
+  std::vector<std::size_t> indicies; // NOTE: CCW index list of triangles
+};
+
+class Sphere : Mesh {
 public:
   explicit Sphere(float radius = 1, std::size_t sectors = NUM_SECTORS_SPHERE,
                   std::size_t stacks = NUM_STACKS_SPHERE) noexcept;
 
 private:
-  void build_vertices();
-  void build_indicies();
+  void build_vertices() override;
+  void build_indicies() override;
 
   float radius;
   std::size_t sectors; // NOTE: Longitude, # of slices
   std::size_t stacks;  // NOTE: Latitude, # of stacks
+};
 
-  std::vector<glm::vec3> vertices;
-  std::vector<std::size_t> indicies; // NOTE: CCW index list of triangles
+class Circle : Mesh {
+public:
+  explicit Circle(float radius = 1, std::size_t sectors = NUM_SECTORS_CIRCLE);
+
+private:
+  void build_vertices() override;
+  void build_indicies() override;
+
+  float radius;
+  std::size_t sectors;
 };
 
 } // namespace gfx::geom
