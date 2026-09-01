8218fe50da6cc2a93bde07688545bae6ae7f3ab9
uiop <uiop@wasdhjkl.xyz>
2026-09-01 17:42:05 -0400

    gfx/circle: fix build_{vertices,indicies}

--- src/circle.cpp
+++ src/circle.cpp
@@ -27,7 +27,7 @@ void Circle::build_vertices() {
   const float sector_step = glm::two_pi<float>() / sectors;
   vertices_.clear();
   vertices_.push_back({0, 0, 0}); // NOTE: Center
-  for (std::size_t k = 0; k <= sectors; k++) {
+  for (std::size_t k = 0; k < sectors; k++) {
     const float theta = sector_step * k;
     vertices_.push_back(
         glm::vec3(glm::cos(theta) * radius, glm::sin(theta) * radius, 0));
@@ -39,7 +39,7 @@ void Circle::build_indicies() {
   constexpr auto center_idx = 0;
   for (std::size_t i = 0; i < sectors; i++) {
     indicies_.push_back(i + 1);
-    indicies_.push_back((i + 2) % sectors);
+    indicies_.push_back(1 + ((i + 1) % sectors));
     indicies_.push_back(center_idx);
   }
 }
